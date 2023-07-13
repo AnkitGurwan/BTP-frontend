d999f1d7cdda47f9c982246f8e246099615297e5
AnkitGurwan <ankitgurwan083@gmail.com>
2023-07-13 14:55:12 +0530

    Update ItemState.js

--- src/context/project/ItemState.js
+++ src/context/project/ItemState.js
@@ -175,7 +175,7 @@ const ItemState=(props)=>{
     const logout=async()=>{
         
         const tenantID = process.env.MICROSOFT_GRAPH_TENANT_ID;
-        const logoutEndpoint = `https://login.microsoftonline.com/${tenantID}/oauth2/v2.0/logout?post_logout_redirect_uri=https://btp-frontend-chi.vercel.app/`;
+        const logoutEndpoint = `https://login.microsoftonline.com/${tenantID}/oauth2/v2.0/logout?post_logout_redirect_uri=http://localhost:3000/`;
         window.location.href = logoutEndpoint;
     }
     
